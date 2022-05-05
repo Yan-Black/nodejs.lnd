be69f3d355f81b2225ed695b83f23e1b21b67c8c
Yan Andryshkin <Yan_Andryshkin@epam.com>
2022-05-05 19:09:50 +0400

    feature: implement task1.2

--- src/task1/task1.2/index.js
+++ src/task1/task1.2/index.js
@@ -1,11 +1,11 @@
 import csv from "csvtojson";
 import { fileURLToPath } from "url";
+import { createReadStream, createWriteStream } from "node:fs";
 
-const csvFilePath = fileURLToPath(new URL("csv/data.xlsx", import.meta.url));
+const csvFilePath = fileURLToPath(new URL("csv/data.csv", import.meta.url));
+const txtFilePath = fileURLToPath(new URL("txt/data.txt", import.meta.url));
 
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonArrayObj) => {
-    //when parse finished, result will be emitted here.
-    console.log(jsonArrayObj);
-  });
+const readableStream = createReadStream(csvFilePath);
+const writableStream = createWriteStream(txtFilePath);
+
+readableStream.pipe(csv()).pipe(writableStream);
