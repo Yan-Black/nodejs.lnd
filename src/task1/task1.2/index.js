7dd1d4f0ddbeff9900dcec689aa6f809efb5d5b7
Yan Andryshkin <Yan_Andryshkin@epam.com>
2022-05-14 17:30:15 +0400

    refactor: use pipiline method instead of streamObj.pipe

--- src/task1/task1.2/index.js
+++ src/task1/task1.2/index.js
@@ -2,6 +2,7 @@ import csv from "csvtojson";
 import { fileURLToPath } from "url";
 import { createReadStream, createWriteStream } from "node:fs";
 import { jsonKeysTransform } from "./jsonKeysTransform";
+import { pipeline } from "stream";
 
 const csvFilePath = fileURLToPath(
   new URL("../../../src/task1/task1.2/csv/data.csv", import.meta.url)
@@ -11,14 +12,12 @@ const txtFilePath = fileURLToPath(new URL("data.txt", import.meta.url));
 const readableStream = createReadStream(csvFilePath);
 const writableStream = createWriteStream(txtFilePath);
 
-readableStream
-  .pipe(csv())
-  .on("error", console.error)
-  .pipe(jsonKeysTransform)
-  .on("error", console.error)
-  .pipe(writableStream)
-  .on("error", console.error)
-  .on("finish", () => {
-    console.log("csv has been processed");
-    process.exit();
-  });
+pipeline(readableStream, csv(), jsonKeysTransform, writableStream, (error) => {
+  if (error) {
+    console.error(error.message);
+    return;
+  }
+
+  console.log("csv has been processed");
+  process.exit();
+});
