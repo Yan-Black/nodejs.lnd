bab31700004eb7d7545023723e564e9680ea46fb
Yan Andryshkin <Yan_Andryshkin@epam.com>
2022-06-17 11:40:24 +0400

    refactor: rename seed imports

--- src/users/seedData/index.cjs
+++ src/users/seedData/index.cjs
@@ -1,13 +1,16 @@
 const { v4: uuidv4 } = require('uuid');
-const { generate } = require('generate-password');
-const { uniqueNamesGenerator, names } = require('unique-names-generator');
+const { generate: generatePassword } = require('generate-password');
+const {
+  uniqueNamesGenerator: generateLogin,
+  names
+} = require('unique-names-generator');
 
 const getRandomAge = () => Math.ceil(Math.random() * 10 + 20);
 
 module.exports = new Array(10).fill(null).map(() => ({
   id: uuidv4(),
-  login: uniqueNamesGenerator({ dictionaries: [names] }),
-  password: generate({ length: 8, numbers: true }),
+  login: generateLogin({ dictionaries: [names] }),
+  password: generatePassword({ length: 8, numbers: true }),
   age: getRandomAge(),
   createdAt: new Date(),
   updatedAt: new Date(),
